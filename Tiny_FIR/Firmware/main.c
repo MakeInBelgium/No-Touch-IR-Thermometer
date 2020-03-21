1055c8a09f89f2a31bcf4781f671ca6690bc7ca5
Kurt De Vocht <kurt.de.vocht@pandora.be>
2020-03-21 08:54:32 +0100

    AtmelStudio 7 project + MLX90614 communication
    AtmelStudio 7 project (Attiny402 start project + I2C)
    Added Adafruit MLX90614 lib logic

--- Tiny_FIR/Firmware/main.c
+++ Tiny_FIR/Firmware/main.c
@@ -1,6 +1,14 @@
-#include "I2C.h"
-#include "avr/io.h"
+#include <atmel_start.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <i2c_simple_master.h>
 
+#define MLX90614_I2CADDR 0x5A
+#define MLX90614_REG_RAWIR1 0x04
+#define MLX90614_REG_RAWIR2 0x05
+#define MLX90614_REG_TA 0x06
+#define MLX90614_REG_TOBJ1 0x07
+#define MLX90614_REG_TOBJ2 0x08
 
 #define LED_OK_bm	PIN3_bm
 #define LED_WARN_bm	PIN6_bm
@@ -11,18 +19,24 @@
 #define BAD  2
 #define ERROR 3
 
+// Function declarations
+void pinSetup();
+void setLEDstatus(uint8_t status);
+float readTemp(uint8_t reg);
+float readObjectTempC(void);
+
 int main(void)
 {
-    // When we arive here it means we just had a reset-event. Most-probably because the user pushed the button!
+	    // When we arive here it means we just had a reset-event. Most-probably because the user pushed the button!
 
-    // 1. Init our little MCU: pins, drivers, possible Microchip middleware if there is no other way.
-	pinSetup();
-	I2C_init();
-	
-    // 2. Do a few measurements for 1 second and average.
-    // 3. Maybe some calculations on the data. FLOATING POINT MATH IS STRICKLY OFF-LIMITS! Why? Because I say so! ;*) 
-    // 4. Display the status on the LED's for a couple of seconds.
-    // 5. Go to sleep and wait for next MCU reset.
+	    // 1. Init our little MCU: pins, drivers, possible Microchip middleware if there is no other way.
+	    pinSetup();
+	    atmel_start_init();
+	    
+	    // 2. Do a few measurements for 1 second and average.
+	    // 3. Maybe some calculations on the data. FLOATING POINT MATH IS STRICKLY OFF-LIMITS! Why? Because I say so! ;*)
+	    // 4. Display the status on the LED's for a couple of seconds.
+	    // 5. Go to sleep and wait for next MCU reset.
 }
 
 void pinSetup(){
@@ -36,13 +50,28 @@ void setLEDstatus(uint8_t status){
 		case WARN: PORTA.OUTSET = LED_WARN_bm ; break;
 		case BAD: PORTA.OUTSET = LED_NOK_bm; break;
 		case ERROR: // Let's blink the NOK and WARN led fast to inform the user something went horribly wrong!
-			for (uint8_t i = 0; i<10;i++)
-			{
-				PORTA.OUTSET = LED_WARN_bm | LED_NOK_bm;
-				_delay_ms(100);
-				PORTA.OUTCLR = LED_WARN_bm | LED_NOK_bm;
-				_delay_ms(100);
-			}
-			break;
+		for (uint8_t i = 0; i<10;i++)
+		{
+			PORTA.OUTSET = LED_WARN_bm | LED_NOK_bm;
+			_delay_ms(100);
+			PORTA.OUTCLR = LED_WARN_bm | LED_NOK_bm;
+			_delay_ms(100);
+		}
+		break;
 	}
+}
+
+float readObjectTempC(void)
+{
+	return readTemp(MLX90614_REG_TOBJ1);
+}
+
+float readTemp(uint8_t reg)
+{
+	float temp;
+	
+	temp = I2C_0_read2ByteRegister(MLX90614_I2CADDR, reg);
+	temp *= .02;
+	temp  -= 273.15;
+	return temp;
 }
