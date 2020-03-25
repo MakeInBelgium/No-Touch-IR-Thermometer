9f9536f5c7b984bca9003dc15936df07ae01e5fe
Kurt De Vocht <kurt.de.vocht@pandora.be>
2020-03-25 09:09:45 +0100

    Switched to continuous measurement

--- Mega_FIR/Firmware/Mega_FIR_Firmware/Mega_FIR_Firmware.ino
+++ Mega_FIR/Firmware/Mega_FIR_Firmware/Mega_FIR_Firmware.ino
@@ -8,10 +8,11 @@
 #define PIN_LED_NOK                   A3 // Red LED
 #define PIN_LOW_FOR_TRIGGER           A4 // When pulled low, a new measurement cycle is triggered (can be used to connect a push-button)
 
-#define STATUS_OK   0
-#define STATUS_WARN   1
-#define STATUS_NOK    2
-#define STATUS_ERROR  3
+#define STATUS_OK             0
+#define STATUS_WARN           1
+#define STATUS_NOK            2
+#define STATUS_ERROR          3
+#define STATUS_INITIALIZING   4
 
 /*
  * Temperature tresholds:
@@ -27,8 +28,11 @@ const uint16_t TEMP_TH_OK =       ( ( 36.5 + 273.15 ) * 50 ) + 0.5;
 const uint16_t TEMP_TH_WARN =     ( ( 38.0 + 273.15 ) * 50 ) + 0.5;
 
 // Globals
-bool serialMonitoring;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+bool serialMonitoring;    // If true, monitoring messages are sent over the serial port
+uint16_t statuses [50];  // Keep last x statuses
+uint16_t numStatuses;     // The number of elements in the array
+uint16_t idxStatuses;     // The index to which the current measurement will be written
 
 void setup(){
 
@@ -42,40 +46,35 @@ void setup(){
   if( !digitalRead( PIN_LOW_FOR_SERIAL_MONITORING ) ){
     serialMonitoring = true;
     Serial.begin( 9600 );
-    while(!Serial);
-    Serial.println( "Mega_FIR - Starting" );
   }
 
-  // Do one measurement when the arduino starts
-  doMeasurement();
+  // Initialize statuses array
+  idxStatuses = 0;
+  numStatuses = sizeof( statuses ) / sizeof( uint16_t );
+  for( uint16_t i = 0; i < numStatuses; i++ ){
+    statuses[i] = STATUS_INITIALIZING;
+  }
 }
 
 void loop(){
-  // Wait for the button to be pressed to start a new measurement cycle
-  while( digitalRead( PIN_LOW_FOR_TRIGGER ) );
-  doMeasurement();
-}
+  // Get the current sensor value
+  uint16_t obj1TempRaw = readTempObj1Raw();
+  if( serialMonitoring ) serialPrintMeasurement( obj1TempRaw );
 
-void doMeasurement(){
-  // Do 5 measurements, keep max
-  uint16_t numMeasurements = 5;
-  uint16_t obj1TempRawMax = 0;
-  setLedAllOn();
-  for( uint16_t i = 0; i < numMeasurements; i++ ){
-    uint16_t obj1TempRaw = readTempObj1Raw();
-    obj1TempRawMax = max( obj1TempRawMax, obj1TempRaw );
-    if( serialMonitoring ) serialPrintMeasurement(obj1TempRaw, i + 1);
-    if( i < numMeasurements - 1 ){
-      delay( 200 );
-    }
+  // Calculate & store a status
+  uint8_t status = getStatus( obj1TempRaw );
+  statuses[idxStatuses] = status;
+  idxStatuses = (idxStatuses + 1 ) % numStatuses;
+
+  // Calculate the worst of last x measurements
+  uint8_t worst = STATUS_OK;
+  for( int i = 0; i < numStatuses; i++ ){
+    worst = max( worst, statuses[i] );
   }
-  if( serialMonitoring ) serialPrintMax( obj1TempRawMax );
-    
-  // Calculate the status
-  uint8_t status = getStatus( obj1TempRawMax );
-        
-  // Display the status on the LED's for a couple of seconds.
-  setLedStatus( status );
+  
+  // Set some LEDs
+  displayStatus( worst );
+  delay( 10 );
 }
 
 void pinSetup(){
@@ -86,99 +85,88 @@ void pinSetup(){
   pinMode( PIN_LED_NOK, OUTPUT );
 }
 
-void setLedAllOff(){
-  digitalWrite( PIN_LED_OK, LOW );
-  digitalWrite( PIN_LED_WARN, LOW );
-  digitalWrite( PIN_LED_NOK, LOW );
-}
-
-void setLedAllOn(){
-  digitalWrite( PIN_LED_OK, HIGH );
-  digitalWrite( PIN_LED_WARN, HIGH );
-  digitalWrite( PIN_LED_NOK, HIGH );
-}
-
-void setLedStatus(uint8_t status){
-  setLedAllOff();
-  
-  switch(status){
-    case STATUS_OK:{
-      digitalWrite( PIN_LED_OK, HIGH );
-      delay(2000);
-      break;
-    }
-    
-    case STATUS_WARN:{
-       digitalWrite( PIN_LED_WARN, HIGH );
-       delay(2000);
-       break;
-    }
-    case STATUS_NOK:{
-      digitalWrite( PIN_LED_NOK, HIGH );
-      delay(2000);
-      break;
-    }
-    
-    case STATUS_ERROR:{ 
-      // Let's blink the NOK and WARN led fast to inform the user something went horribly wrong!
-      for (uint8_t i = 0; i < 15; i++) {
-        setLedAllOn();
-        _delay_ms(100);
-        setLedAllOff();
-        _delay_ms(100);
-      }
-      break;
-    }
-  }
+uint16_t readTempObj1Raw(){
+  return mlx.read16(MLX90614_TOBJ1);
 }
 
 uint8_t getStatus( uint16_t rawValue ){
   // MSB = 1?  => Sensor read error (Datasheet p. 30)
   if( ( rawValue & MLX90614_ERROR_BIT ) == MLX90614_ERROR_BIT){
-    if( serialMonitoring ) Serial.println( "Sensor returned error => STATUS_ERROR" );
+    if( serialMonitoring ) Serial.println( " - Sensor error => STATUS_ERROR" );
     return STATUS_ERROR;
   }
   
   if( rawValue <= TEMP_TH_TOO_LOW ){
-    if( serialMonitoring ) Serial.println( "Temperature too low => STATUS_ERROR" );
+    if( serialMonitoring ) Serial.println( " - T too low => STATUS_ERROR" );
     return STATUS_ERROR;
   }
   
   if( rawValue <= TEMP_TH_OK ){
-    if( serialMonitoring ) Serial.println( "Temperature ok => STATUS_OK" );
+    if( serialMonitoring ) Serial.println( " - T ok => STATUS_OK" );
     return STATUS_OK;
   }
   
   if( rawValue <= TEMP_TH_WARN ){
-    if( serialMonitoring ) Serial.println( "Temperature above ok => STATUS_WARN" );
+    if( serialMonitoring ) Serial.println( " - T above ok => STATUS_WARN" );
     return STATUS_WARN;
   }
-  if( serialMonitoring ) Serial.println( "Temperature above warning => STATUS_NOK" );
+  if( serialMonitoring ) Serial.println( " - T above warning => STATUS_NOK" );
   return STATUS_NOK;
 }
 
-uint16_t readTempObj1Raw(){
-  return mlx.read16(MLX90614_TOBJ1);
+void displayStatus( uint8_t status ){
+  switch(status){
+    case STATUS_OK:{
+      digitalWrite( PIN_LED_OK, HIGH );
+      digitalWrite( PIN_LED_WARN, LOW );
+      digitalWrite( PIN_LED_NOK, LOW );
+      break;
+    }
+
+    case STATUS_WARN:{
+      digitalWrite( PIN_LED_OK, LOW );
+      digitalWrite( PIN_LED_WARN, HIGH );
+      digitalWrite( PIN_LED_NOK, LOW );
+      break;
+    }
+
+    case STATUS_NOK:{
+      digitalWrite( PIN_LED_OK, LOW );
+      digitalWrite( PIN_LED_WARN, LOW );
+      digitalWrite( PIN_LED_NOK, HIGH );
+      break;
+    }
+    
+    case STATUS_ERROR:{
+      // Make the warning LED flash
+      uint32_t m = millis() % 200;
+      bool ledIsOn = m < 20;
+      digitalWrite( PIN_LED_OK, LOW );
+      digitalWrite( PIN_LED_WARN, ledIsOn );
+      digitalWrite( PIN_LED_NOK, LOW );
+      break;
+    }
+
+    case STATUS_INITIALIZING:{
+      // Make the warning LED flash
+      uint32_t m = millis() % 200;
+      digitalWrite( PIN_LED_OK, m < 20 );
+      digitalWrite( PIN_LED_WARN, m >= 66 && m < 86 );
+      digitalWrite( PIN_LED_NOK, m >= 132 && m < 152 );
+      break;
+    }
+  }
+}
+
+void serialPrintMeasurement( uint16_t tempRaw ){
+  Serial.print( millis() );
+  Serial.print( " - Sensor value: " );
+  Serial.print( tempRaw ); 
+  Serial.print( " = ");
+  Serial.print( rawTempToCelcius( tempRaw ) );
+  Serial.print( "°C " );
 }
 
 double rawTempToCelcius( uint16_t raw ){
   return raw * 0.02 - 273.15;
-}
-
-void serialPrintMeasurement( uint16_t tempRaw, uint16_t i ){
-  Serial.print( "Sensor value " );
-  Serial.print( i );
-  Serial.print( ": " );
-  Serial.print( tempRaw ); 
-  Serial.print( " = ");
-  Serial.print( rawTempToCelcius( tempRaw ) );
-  Serial.println( "°C" );
-}
-
-void serialPrintMax( uint16_t tempMax ){
-  Serial.print( " => Maximum value: " );
-  Serial.print( tempMax ); 
-  Serial.print(" = ");
-  Serial.print( rawTempToCelcius( tempMax ) );
-  Serial.println( "°C" );
 }
